47e895eead037f3e5da0b980c30cac2bd3054435
AMAN02SS <7000602733aman@gmail.com>
2026-09-07 03:50:32 +0530

    feat: create backend storage to integration for CI/CD

--- terraform/environment/dev/backend.tf
+++ terraform/environment/dev/backend.tf
@@ -3,7 +3,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
-    storage_account_name = "sttfstatedev01"
+    storage_account_name = "sttfstatedev0709"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
   }
--- terraform/environment/prod/backend.tf
+++ terraform/environment/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
-    storage_account_name = "sttfstatedev01"
+    storage_account_name = "sttfstateprod0609"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
